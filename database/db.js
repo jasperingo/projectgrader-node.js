0190104c3e5937039938d5bd43c25b7122406838
Jasper Anelechukwu <jasperanels@gmail.com>
2021-09-20 21:34:04 +0100

    Made new changes

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ var db = mysql.createConnection({
 	host     : process.env.DB_HOST,
 	user     : process.env.DB_USER,
 	password : process.env.DB_PASS,
-	database : process.env.DB_TABLE
+	database : process.env.DB_NAME
 });
 
 db.connect((err) => {
